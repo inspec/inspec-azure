9a91669e6869174c251671c1e481ec05536d29fc
Ruairi Fennell <rfennell@chef.io>
2018-07-05 14:33:18 +0100

    Pagination fix for >100 users

--- test/integration/verify/controls/azure_ad_users.rb
+++ test/integration/verify/controls/azure_ad_users.rb
@@ -3,7 +3,7 @@ control 'azure_ad_users' do
 
   title 'Ensure no Guest Users are present within an Active Directory Tenant'
 
-  describe azure_ad_users.each do |user|
+  describe azure_ad_users.map do |user|
     describe azure_ad_user(object_id: nil, user_args: user) do
       it { should_not be_guest }
     end
