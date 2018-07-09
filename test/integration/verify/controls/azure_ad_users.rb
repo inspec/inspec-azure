e2dd81a70f847bc357ac4771dff761d2ac05dffc
Ruairi Fennell <rfennell@chef.io>
2018-07-09 15:04:13 +0100

    PR feedback

--- test/integration/verify/controls/azure_ad_users.rb
+++ test/integration/verify/controls/azure_ad_users.rb
@@ -3,9 +3,18 @@ control 'azure_ad_users' do
 
   title 'Ensure no Guest Users are present within an Active Directory Tenant'
 
-  describe azure_ad_users.table.each {
-    |user| describe azure_ad_user(nil, user) do
-      it { should_not be_guest }
-    end
-  }
+  # TODO which?
+
+  # Slow, API call per User
+  # describe azure_ad_users.data.map{|user| user["objectId"]}.each { |id|
+  #   describe azure_ad_user(id) do
+  #     it { should exist}
+  #     it { should_not be_guest }
+  #   end
+  # }
+
+  # Fast, does not tell which User failed the control.
+  # describe azure_ad_users.data.map{|user| user["userType"]} do
+  #     it { should_not include 'Guest' }
+  # end
 end
