5ad836f14e98b4c5ff514a3be2096cf52bca1fa4
Joshua Padgett <jpadgett@chef.io>
2018-07-12 13:21:05 -0400

    Rename azure_network_watcher(s) resources to azurerm_network_watcher(s) (#86)
    * Add deprecation warnings to azure_network_watcher(s)
    * Update docs to reference new resource names
    * Update integration tests to use renamed resources
    
    Signed-off-by: Joshua Padgett <jpadgett@chef.io>

--- test/integration/verify/controls/azurerm_network_watchers.rb
+++ test/integration/verify/controls/azurerm_network_watchers.rb
@@ -1,9 +1,9 @@
 resource_group = attribute('resource_group', default: nil)
 
-control 'azure_network_watchers' do
+control 'azurerm_network_watchers' do
   only_if { ENV['ENABLE_NETWORK_WATCHER'] }
 
-  describe azure_network_watchers(resource_group: resource_group) do
+  describe azurerm_network_watchers(resource_group: resource_group) do
     it           { should exist }
     its('names') { should be_an(Array) }
   end
