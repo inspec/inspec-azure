87ece7175760e1bcc4ff86ae7dd04be1cbe6e689
Trevor Bramble <inbox@trevorbramble.com>
2018-07-13 09:57:40 -0700

    Rename azure{,rm}_resource_groups resource (#89)
    Also added `sort` to the docs list generation because directory contents
    are not otherwise ordered deterministically across platforms.
    
    Signed-off-by: Trevor Bramble <tbramble@chef.io>

--- test/integration/verify/controls/azure_resource_groups.rb
+++ test/integration/verify/controls/azure_resource_groups.rb
@@ -1,7 +1,7 @@
 resource_group = attribute('resource_group', default: nil)
 
-control 'azure_resource_groups' do
-  describe azure_resource_groups do
+control 'azurerm_resource_groups' do
+  describe azurerm_resource_groups do
     it           { should exist }
     its('names') { should include(resource_group) }
   end
