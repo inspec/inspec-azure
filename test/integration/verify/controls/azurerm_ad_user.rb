10e1c67b001bfdc887ec4480fee736869b4503cb
Joshua Padgett <jpadgett@chef.io>
2018-07-13 11:40:52 -0400

    Add integration test for azurerm_ad_user resource
    Signed-off-by: Joshua Padgett <jpadgett@chef.io>

---
+++ test/integration/verify/controls/azurerm_ad_user.rb
@@ -0,0 +1,10 @@
+control 'azurerm_ad_user' do
+  user_id = azurerm_ad_users.object_ids.first
+  describe azurerm_ad_user(user_id: user_id) do
+    it                    { should exist }
+    its('objectId')       { should eq user_id }
+    its('displayName')    { should_not be_nil }
+    its('userType')       { should eq 'Member' }
+    its('accountEnabled') { should_not be_nil }
+  end
+end
