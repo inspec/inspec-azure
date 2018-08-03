097920c86a48a108c722bdf7713688a4f0bdefe4
mmclane <matt.mclane@hooklogic.com>
2018-08-03 19:09:49 -0400

    Add azurerm_virtual_network and azurerm_virtual_networks resources (#105)
    Signed-off-by: Matt Mclane <m.mclane@criteo.com>

---
+++ libraries/azurerm_virtual_network.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'azurerm_resource'
+
+class AzurermVirtualNetwork < AzurermSingularResource
+  name 'azurerm_virtual_network'
+  desc 'Verifies settings for an Azure Virtual Network'
+  example <<-EXAMPLE
+    describe azurerm_virtual_network(resource_group: 'example', name: 'vnet-name') do
+      it { should have_monitoring_agent_installed }
+    end
+  EXAMPLE
+
+  ATTRS = %i(
+    id
+    name
+    location
+    type
+    tags
+    properties
+  ).freeze
+
+  attr_reader(*ATTRS)
+
+  def initialize(resource_group: nil, name: nil)
+    resp = client.virtual_network(resource_group, name)
+    return if resp.nil? || resp.key?('error')
+
+    ATTRS.each do |field|
+      instance_variable_set("@#{field}", resp[field.to_s])
+    end
+
+    @subs = Array(resp['properties']['subnets']) || []
+
+    @exists = true
+  end
+
+  def address_space
+    properties['addressSpace']['addressPrefixes']
+  end
+
+  def dns_servers
+    properties['dhcpOptions']['dnsServers']
+  end
+
+  def vnet_peerings
+    name_id = ->(peer) { [peer['name'], peer.dig('properties', 'remoteVirtualNetwork', 'id')] }
+    any_nils = ->(pair) { pair.any?(&:nil?) }
+
+    properties['virtualNetworkPeerings'].collect(&name_id).reject(&any_nils).to_h
+  end
+
+  def enable_ddos_protection
+    properties['enableDdosProtection']
+  end
+
+  def enable_vm_protection
+    properties['enableVmProtection']
+  end
+
+  def subnets
+    @subs.collect { |s| s['name'] }
+  end
+
+  def to_s
+    "'#{name}' Virtual Network"
+  end
+end
