b711e0f5548a15b59226f651cff58573d67b22f0
Ruairi Fennell <rfennell@chef.io>
2018-08-21 14:22:01 +0100

    Update User Agent
    Signed-off-by: Ruairi Fennell <rfennell@chef.io>

--- libraries/support/azure/rest.rb
+++ libraries/support/azure/rest.rb
@@ -6,8 +6,7 @@ require 'faraday_middleware'
 module Azure
   class Rest
     USER_AGENT = 'User-Agent'
-    INSPEC_USER_AGENT = 'Chef-Inspec-pid-18d63047-6cdf-4f34-beed-62f01fc73fc2'
-    private_constant  :USER_AGENT, :INSPEC_USER_AGENT
+    INSPEC_USER_AGENT = 'Chef-Inspec-18d63047-6cdf-4f34-beed-62f01fc73fc2'
 
     attr_reader :host, :resource, :credentials
 
