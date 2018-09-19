3c4fa897ce66a07d33fa79ea98223f11727ea0f5
Ruairi Fennell <rfennell@chef.io>
2018-09-19 09:53:28 +0100

    Change PID
    Signed-off-by: Ruairi Fennell <rfennell@chef.io>

--- libraries/support/azure/rest.rb
+++ libraries/support/azure/rest.rb
@@ -6,7 +6,7 @@ require 'faraday_middleware'
 module Azure
   class Rest
     USER_AGENT        = 'User-Agent'
-    INSPEC_USER_AGENT = 'Inspec-18d63047-6cdf-4f34-beed-62f01fc73fc2'
+    INSPEC_USER_AGENT = 'pid-18d63047-6cdf-4f34-beed-62f01fc73fc2'
 
     attr_reader :host, :resource, :credentials
 
