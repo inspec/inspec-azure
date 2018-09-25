03b9d593096ca4824d849506aad27f74272ffe0d
David McCown <dmccown@users.noreply.github.com>
2018-09-25 11:20:41 -0700

    Exempts tags from being turned into a struct (#140)
    * Exempts tags from being turned into a struct
    
    Turning tags into a struct makes it difficult to compare the expected
    sets of tags with the actual set of tags returned. There's also the
    potential for dashes in tag names. In these cases the struct methods
    are only accessible by calling `send` on the struct with the key names.
    
    This change preserves tags as a hash.
    
    fixes #138
    
    Signed-off-by: David McCown <dmccown@chef.io>

--- test/unit/support/azure/service_test.rb
+++ test/unit/support/azure/service_test.rb
@@ -3,8 +3,10 @@ require_relative '../../../../libraries/support/azure/service'
 
 describe Azure::Service do
   let(:address)         { { street: '123 Fake St.', city: 'Nowhere', state: 'NO' } }
+  let(:tags)            { { 'tag-1' => 'my-custom-tag', 'tag-2' => 'my-other-tag' } }
   let(:john)            { { name: 'John', age: 20, address: address } }
   let(:jane)            { { name: 'Jane', age: 25, address: {} } }
+  let(:doe)             { { name: 'Doe', age: 30, address: {}, tags: tags } }
   let(:array_of_hashes) { [john, jane] }
 
   before do
@@ -36,5 +38,11 @@ describe Azure::Service do
     assert_equal('Jane', struct.name)
     assert_equal(25, struct.age)
     assert_equal({}, struct.address)
+  end
+
+  it 'preserves tags key' do
+    struct = @service.to_struct(doe)
+
+    assert_equal(tags, struct.tags)
   end
 end
