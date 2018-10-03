2181d9dee51bb22e8a441f2a015e82ec2bf85468
David McCown <dmccown@chef.io>
2018-10-03 11:39:49 -0500

    Scopes storage_accounts resource by resource group
    When trying to iterate over all resource groups and storage accounts we
    currently can only ask for all storage accounts. This means we try to
    look up every storage account for every resource group.
    
    This fix allows one to scope their storage accounts query by resource
    group.
    
    Removes rake target we no longer need.
    
    Signed-off-by: David McCown <dmccown@chef.io>

--- test/integration/verify/controls/azurerm_storage_accounts.rb
+++ test/integration/verify/controls/azurerm_storage_accounts.rb
@@ -1,7 +1,12 @@
+resource_group = attribute('resource_group', default: nil)
 storage_account = attribute('storage_account', default: nil)
 
 control 'azurerm_storage_accounts' do
   describe azurerm_storage_accounts do
+    its('names') { should include(storage_account) }
+  end
+
+  describe azurerm_storage_accounts(resource_group: resource_group) do
     its('names') { should include(storage_account) }
   end
 end
