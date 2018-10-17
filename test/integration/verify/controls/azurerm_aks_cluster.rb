3419ca87f28cfad40023bcd91da0eda5dbba8857
James Massardo <jmassardo@chef.io>
2018-10-17 10:05:17 -0500

    Make rubocop happy.
    Signed-off-by: James Massardo <jmassardo@chef.io>

--- test/integration/verify/controls/azurerm_aks_cluster.rb
+++ test/integration/verify/controls/azurerm_aks_cluster.rb
@@ -6,16 +6,16 @@ control 'azurerm_aks_cluster' do
     it                          { should exist }
     its('name')                 { should cmp 'inspecakstest' }
     its('type')                 { should cmp 'Microsoft.ContainerService/managedClusters' }
-    its('provisioning_state')   { should cmp "Succeeded" }
-    its('kubernetes_version')   { should cmp "1.9.9" }
+    its('provisioning_state')   { should cmp 'Succeeded' }
+    its('kubernetes_version')   { should cmp '1.9.9' }
     its('dns_prefix')           { should cmp 'inspecaksagent1' }
     its('fqdn')                 { should cmp cluster_fqdn }
     its('pool_name')            { should cmp 'inspecakstest' }
-    its('pool_count')           { should cmp "5" }
-    its('pool_vm_size')         { should cmp "Standard_DS1_v2" }
-    its('pool_storage_profile') { should cmp "ManagedDisks" }
-    its('pool_max_pods')        { should cmp "110" }
-    its('pool_os_type')         { should cmp "Linux" }
+    its('pool_count')           { should cmp '5' }
+    its('pool_vm_size')         { should cmp 'Standard_DS1_v2' }
+    its('pool_storage_profile') { should cmp 'ManagedDisks' }
+    its('pool_max_pods')        { should cmp '110' }
+    its('pool_os_type')         { should cmp 'Linux' }
   end
 
   describe azurerm_aks_cluster(resource_group: resource_group, name: 'fake') do
