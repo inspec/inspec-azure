3bfe11310db65261104cadc94b55add4f4a46bcb
Ruairi Fennell <rfennell@chef.io>
2018-10-17 18:53:10 +0100

    Add ability to check if a Storage Account has regenerated its Access Key in the last 90 days
    Signed-off-by: Ruairi Fennell <rfennell@chef.io>

--- libraries/azurerm_storage_account.rb
+++ libraries/azurerm_storage_account.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'azurerm_resource'
+require 'date'
+require 'time'
 
 class AzurermStorageAccout < AzurermSingularResource
   name 'azurerm_storage_account'
@@ -21,17 +23,36 @@ class AzurermStorageAccout < AzurermSingularResource
   attr_reader(*ATTRS)
 
   def initialize(name: 'default', resource_group: nil)
-    resp = management.storage_account(resource_group, name)
-    return if has_error?(resp)
+    storage_account = management.storage_account(resource_group, name)
+    return if has_error?(storage_account)
 
-    @name       = resp.name
-    @id         = resp.id
-    @properties = resp.properties
+    assign_fields(ATTRS, storage_account)
 
     @exists = true
   end
 
+  def have_recently_generated_access_key
+    now = DateTime.now
+    less_than = to_utc(now)
+    greater_than = to_utc(now-90)
+
+    filter = "resourceId eq '#{id}' and "\
+             "eventTimestamp ge '#{greater_than}' and "\
+             "eventTimestamp le '#{less_than}' and "\
+             "operations eq 'Microsoft.Storage/storageAccounts/regeneratekey/action'"
+
+    log_events = management.activity_log_alert_filtered(filter)
+    log_events.any?
+  end
+
   def to_s
     "#{name} Storage Account"
+  end
+
+  private
+
+  def to_utc(datetime)
+    # API requires times in UTC ISO8601 format.
+    datetime.to_time.utc.iso8601
   end
 end
