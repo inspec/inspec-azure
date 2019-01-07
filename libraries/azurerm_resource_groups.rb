be2cd02c07a8d8de8a285974d218e2c85c075aef
Stuart Preston <stuart@chef.io>
2019-01-07 15:45:33 -0800

    Corrections to examples
    Signed-off-by: Stuart Preston <stuart@chef.io>

--- libraries/azurerm_resource_groups.rb
+++ libraries/azurerm_resource_groups.rb
@@ -6,7 +6,7 @@ class AzurermResourceGroups < AzurermPluralResource
   name 'azurerm_resource_groups'
   desc 'Fetches all available resource groups'
   example <<-EXAMPLE
-    describe azure_rmresource_groups do
+    describe azurerm_resource_groups do
       its('names') { should include('example-group') }
     end
   EXAMPLE
