a0f8d273a8c2c07c2dc6b28489fdecdf75977e19
Ruairi Fennell <rfennell@chef.io>
2019-07-03 16:24:22 +0100

    Remove broken policy check
    Signed-off-by: Ruairi Fennell <rfennell@chef.io>

--- test/integration/verify/controls/azurerm_security_center_policies.rb
+++ test/integration/verify/controls/azurerm_security_center_policies.rb
@@ -1,13 +1,7 @@
-resource_group = attribute('resource_group', default: nil)
-
 control 'azurerm_security_center_policies' do
   describe azurerm_security_center_policies do
     it                  { should exist }
-    its('policy_names') { should include('default', resource_group) }
+    its('policy_names') { should include('default') }
   end
 
-  describe azurerm_security_center_policies.where(name: resource_group) do
-    it                  { should exist }
-    its('policy_names') { should eq([resource_group]) }
-  end
 end
