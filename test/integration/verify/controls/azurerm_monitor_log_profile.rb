1ba38ab60d3aba12750205d97df82e768effd12c
Ruairi Fennell <rfennell@chef.io>
2019-10-15 11:18:41 +0100

    Prevent deletion of Log Profile on CI Destroy
    Signed-off-by: Ruairi Fennell <rfennell@chef.io>

--- test/integration/verify/controls/azurerm_monitor_log_profile.rb
+++ test/integration/verify/controls/azurerm_monitor_log_profile.rb
@@ -1,5 +1,7 @@
+log_profile = attribute('log_profile_name', default: nil)
+
 control 'azurerm_monitor_log_profile' do
-  describe azurerm_monitor_log_profile(name: 'default') do
+  describe azurerm_monitor_log_profile(name: log_profile) do
     it { should exist }
     its('retention_enabled') { should be true }
     its('retention_days')    { should eq(365) }
