205803bb6a2836a5022dbcbb061c3492ec7243dd
Mikael Krief <krief_mikael@hotmail.com>
2019-10-23 17:26:18 +0200

    fix blank lines
    Signed-off-by: Mikael Krief <krief_mikael@hotmail.com>

--- test/integration/verify/controls/azurerm_mysql_server.rb
+++ test/integration/verify/controls/azurerm_mysql_server.rb
@@ -3,8 +3,6 @@ mysql_server_name = attribute('mysql_server_name', default: nil)
 
 control 'azurerm_mysql_server' do
   only_if { ENV['SQL'] }
-
-
   describe azurerm_mysql_server(resource_group: resource_group, server_name: mysql_server_name) do
     it                { should exist }
     its('id')         { should_not be_nil }
