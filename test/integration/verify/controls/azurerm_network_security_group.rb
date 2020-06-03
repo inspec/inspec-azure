e4cc8e1a9744f69cce83291abba6e4c0a2687e0e
Gavin Reynolds <gavin@chef.io>
2020-06-03 19:25:14 +0100

    Add integration tests for network security group rules, using a wide variety of source address prefix(es) and destination port range(s)
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- test/integration/verify/controls/azurerm_network_security_group.rb
+++ test/integration/verify/controls/azurerm_network_security_group.rb
@@ -1,6 +1,8 @@
 resource_group = input('resource_group',            value: nil)
 nsg            = input('network_security_group',    value: nil)
 nsg_id         = input('network_security_group_id', value: nil)
+nsg_insecure   = input('network_security_group_insecure', value: nil)
+nsg_open       = input('network_security_group_open', value: nil)
 
 control 'azurerm_network_security_group' do
   describe azurerm_network_security_group(resource_group: resource_group, name: nsg) do
@@ -12,6 +14,38 @@ control 'azurerm_network_security_group' do
     its('default_security_rules') { should_not be_empty }
     it                            { should_not allow_rdp_from_internet }
     it                            { should_not allow_ssh_from_internet }
+    it                            { should_not allow_port_from_internet('1433') }
+    it                            { should_not allow_port_from_internet('1521') }
+    it                            { should_not allow_port_from_internet('4333') }
+    it                            { should_not allow_port_from_internet('5432') }
+    it                            { should_not allow_port_from_internet('139') }
+    it                            { should_not allow_port_from_internet('1433') }
+    it                            { should_not allow_port_from_internet('445') }
+    it                            { should_not allow_port_from_internet('1433') }
+    it                            { should_not allow_port_from_internet('21') }
+    it                            { should_not allow_port_from_internet('69') }
+  end
+
+  describe azurerm_network_security_group(resource_group: resource_group, name: nsg_insecure) do
+    it                            { should exist }
+    it                            { should allow_rdp_from_internet }
+    it                            { should allow_ssh_from_internet }
+    it                            { should allow_port_from_internet('1433') }
+    it                            { should allow_port_from_internet('1521') }
+    it                            { should allow_port_from_internet('4333') }
+    it                            { should allow_port_from_internet('5432') }
+    it                            { should allow_port_from_internet('139') }
+    it                            { should allow_port_from_internet('1433') }
+    it                            { should allow_port_from_internet('445') }
+    it                            { should allow_port_from_internet('1433') }
+    it                            { should allow_port_from_internet('21') }
+    it                            { should allow_port_from_internet('69') }
+  end
+
+  describe azurerm_network_security_group(resource_group: resource_group, name: nsg_open) do
+    it                            { should exist }
+    it                            { should allow_rdp_from_internet }
+    it                            { should allow_ssh_from_internet }
   end
 
   describe azurerm_network_security_group(resource_group: resource_group, name: 'fake') do
