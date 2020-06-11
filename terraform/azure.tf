ff2be4fb5727fb51c27396f94167cab06bc834e6
Gavin Reynolds <gavin@chef.io>
2020-06-11 13:11:37 +0100

    Add application gateway Terraform
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -818,3 +818,112 @@ resource "azurerm_cosmosdb_account" "inspectest_cosmosdb" {
     failover_priority = 0
   }
 }
+
+resource "azurerm_virtual_network" "app-gw" {
+  name                = "app-gw-network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.254.0.0/16"]
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.app-gw.name
+  address_prefix       = "10.254.0.0/24"
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "backend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.app-gw.name
+  address_prefix       = "10.254.2.0/24"
+}
+
+resource "azurerm_public_ip" "test" {
+  name                = "example-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+# since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.app-gw.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.app-gw.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.app-gw.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.app-gw.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.app-gw.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.app-gw.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.app-gw.name}-rdrcfg"
+}
+
+resource "azurerm_application_gateway" "network" {
+  name                = "example-appgateway"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku {
+    name     = "Standard_Small"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.frontend.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 443
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.test.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 1
+  }
+
+  ssl_certificate {
+    name = "inspec.example.com"
+    data = filebase64("app-gw/inspec.example.com.pfx")
+    password = "InSpec1234"
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Https"
+    ssl_certificate_name           = "inspec.example.com"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+
+  ssl_policy {
+    # https://docs.microsoft.com/en-us/azure/application-gateway/application-gateway-ssl-policy-overview
+    # disabled_protocols   = ["TLSv1_0", "TLSv1_1"]
+    # min_protocol_version = "TLSv1_2"
+    policy_name          = "AppGwSslPolicy20170401S"
+    policy_type          = "Predefined"
+  }
+}
