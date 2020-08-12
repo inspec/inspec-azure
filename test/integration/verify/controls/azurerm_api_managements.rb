c50d67050545190f3f9a63cf6c3950fed9cef87a
Omer Demirok <odemirok@chef.io>
2020-08-12 12:47:29 +0100

    Fix azure file failing the integration tests
    Signed-off-by: Omer Demirok <odemirok@chef.io>

--- test/integration/verify/controls/azurerm_api_managements.rb
+++ test/integration/verify/controls/azurerm_api_managements.rb
@@ -1,8 +1,8 @@
 resource_group = attribute('resource_group', default: nil)
-api_management_name = attribute('api_management_name', default: nil)
+api_management_name = attribute('api_management_name', default: '')
 
 control 'azurerm_api_managements' do
-
+  only_if { !api_management_name.empty? }
   describe azurerm_api_managements(resource_group: resource_group) do
     it           { should exist }
     its('names') { should include api_management_name }
