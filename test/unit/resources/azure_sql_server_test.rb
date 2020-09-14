3b596b01c96f1fcb17aaaf91bf677a38e11cb8c1
Omer Demirok <odemirok@chef.io>
2020-09-14 15:33:46 +0100

    Fix unit tests
    Signed-off-by: Omer Demirok <odemirok@chef.io>

--- test/unit/resources/azure_sql_server_test.rb
+++ test/unit/resources/azure_sql_server_test.rb
@@ -3,15 +3,15 @@ require 'azure_sql_server'
 
 class AzureSqlServerConstructorTest < Minitest::Test
   def test_empty_param_not_ok
-    assert_raises(ArgumentError) { AzureSqlServer }
+    assert_raises(ArgumentError) { AzureSqlServer.new }
   end
 
   # resource_provider should not be allowed.
   def test_resource_provider_not_ok
-    assert_raises(ArgumentError) { AzureSqlServer(resource_provider: 'some_type') }
+    assert_raises(ArgumentError) { AzureSqlServer.new(resource_provider: 'some_type') }
   end
 
   def test_resource_group
-    assert_raises(ArgumentError) { AzureSqlServer(name: 'my-name') }
+    assert_raises(ArgumentError) { AzureSqlServer.new(name: 'my-name') }
   end
 end
