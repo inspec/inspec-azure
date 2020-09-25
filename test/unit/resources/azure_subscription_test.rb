39e975f77df1a00c4f73ddd20f5944d94491ec49
Omer Demirok <odemirok@chef.io>
2020-09-25 12:35:35 +0100

    Add azure_subscription(s) resources
    Signed-off-by: Omer Demirok <odemirok@chef.io>

---
+++ test/unit/resources/azure_subscription_test.rb
@@ -0,0 +1,13 @@
+require_relative 'helper'
+require 'azure_subscription'
+
+class AzureSubscriptionConstructorTest < Minitest::Test
+  # resource_provider should not be allowed.
+  def test_resource_provider_not_ok
+    assert_raises(ArgumentError) { AzureSubscription.new(resource_provider: 'some_type') }
+  end
+
+  def test_name_not_ok
+    assert_raises(ArgumentError) { AzureSubscription.new(name: 'some_name') }
+  end
+end
