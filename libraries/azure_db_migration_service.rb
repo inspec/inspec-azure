41a85e9c5754c99e12e9dc3d86760fd9ddff8e4d
Sathish <sbabu@progress.com>
2021-06-09 10:50:50 +0530

    support db migration service(s)
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ libraries/azure_db_migration_service.rb
@@ -0,0 +1,23 @@
+require 'azure_generic_resource'
+
+class AzureDBMigrationService < AzureGenericResource
+  name 'azure_db_migration_service'
+  desc 'Verifies settings for a DB migration service resource in a resource group'
+  example <<-EXAMPLE
+    describe azure_db_migration_service(resource_group: 'rg-1, service_name: 'DmsSdkService') do#{' '}
+      it { should exist }
+    end
+  EXAMPLE
+
+  def initialize(opts = {})
+    raise ArgumentError, 'Parameters must be provided in an Hash object.' unless opts.is_a?(Hash)
+
+    opts[:resource_provider] = specific_resource_constraint('Microsoft.DataMigration/services', opts)
+    opts[:resource_identifiers] = %i(service_name)
+    super(opts, true)
+  end
+
+  def to_s
+    super(AzureDBMigrationService)
+  end
+end
---
+++ libraries/azure_db_migration_services.rb
@@ -0,0 +1,59 @@
+require 'azure_generic_resource'
+
+class AzureDBMigrationServices < AzureGenericResources
+  name 'azure_db_migration_services'
+  desc 'Verifies settings for a list of DB migration service resources in a resource group'
+  example <<-EXAMPLE
+    describe azure_db_migration_services do#{' '}
+      it { should exist }
+    end
+  EXAMPLE
+
+  attr_reader :table
+
+  def initialize(opts = {})
+    raise ArgumentError, 'Parameters must be provided in an Hash object.' unless opts.is_a?(Hash)
+
+    opts[:resource_provider] = specific_resource_constraint('Microsoft.DataMigration/services', opts)
+    super
+    return if failed_resource?
+
+    @table.map! do |row|
+      sku = row.delete(:sku)
+      properties = row.delete(:properties)
+      row[:sku_name] = sku[:name]
+      row[:sku_size] = sku[:size]
+      row[:sku_tier] = sku[:tier]
+      row[:sku_capacity] = sku[:capacity]
+      row[:sku_family] = sku[:family]
+      row[:provisioning_state] = properties[:provisioningState]
+      row[:virtual_nic_id] = properties[:virtualNicId]
+      row[:virtual_subnet_id] = properties[:virtualSubnetId]
+      row
+    end
+
+    table_schema = [
+      { column: :ids, field: :id },
+      { column: :names, field: :name },
+      { column: :kinds, field: :kind },
+      { column: :types, field: :type },
+      { column: :etags, field: :etag },
+      { column: :sku_names, field: :sku_name },
+      { column: :sku_sizes, field: :sku_size },
+      { column: :sku_tiers, field: :sku_tier },
+      { column: :sku_capacities, field: :sku_capacity },
+      { column: :sku_families, field: :sku_family },
+      { column: :provisioning_states, field: :provisioning_state },
+      { column: :virtual_nic_ids, field: :virtual_nic_id },
+      { column: :virtual_subnet_ids, field: :virtual_subnet_id },
+      { column: :locations, field: :location },
+      { column: :tags, field: :tags },
+    ]
+
+    AzureGenericResources.populate_filter_table(:table, table_schema)
+  end
+
+  def to_s
+    super(AzureDBMigrationServices)
+  end
+end
