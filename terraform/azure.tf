9930ed0a4fb363b9d16435f7f725f35f4f61e1e3
Sathish <sbabu@progress.com>
2021-06-09 10:53:26 +0530

    fix subnet for upgraded version!
    Signed-off-by: Sathish <sbabu@progress.com>

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -298,8 +298,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.1.1.0/24"
-  # "Soft" deprecated, required until v2 of azurerm provider:
-  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
