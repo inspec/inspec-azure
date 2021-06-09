0e678886ea1ccb3aff2ec1943d944f8a9e80bc5e
Sathish <sbabu@progress.com>
2021-06-09 10:52:15 +0530

    update tf provider version that supports DB migration service
    Signed-off-by: Sathish <sbabu@progress.com>

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -3,11 +3,12 @@ terraform {
 }
 
 provider "azurerm" {
-  version         = "~> 1.36.0"
+  version         = "~> 2.1.0"
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+  features {}
 }
 
 provider "random" {
