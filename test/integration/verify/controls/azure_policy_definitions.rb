797096ea7abba477e72006f60f2cde80d72c21d7
Sathish <sbabu@progress.com>
2021-06-17 11:14:47 +0530

    check policy type in integration test
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_policy_definitions.rb
+++ test/integration/verify/controls/azure_policy_definitions.rb
@@ -1,5 +1,6 @@
 control 'azure_policy_definitions' do
   describe azure_policy_definitions(built_in_only: true) do
     it { should exist }
+    its('policy_types') { should_not include 'Custom' }
   end
 end
