ba23bc24cdd6cc26f4cc2552758115f3f02b93bd
Sathish <sbabu@progress.com>
2021-06-21 08:10:14 +0530

    unit test policy exemption(s)
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ test/unit/resources/azure_policy_exemptions_test.rb
@@ -0,0 +1,12 @@
+require_relative 'helper'
+require 'azure_policy_exemptions'
+
+class AzurePolicyExemptionsConstructorTest < Minitest::Test
+  def tag_value_not_ok
+    assert_raises(ArgumentError) { AzurePolicyExemptions.new(tag_value: 'some_tag_value') }
+  end
+
+  def test_resource_id_alone_not_ok
+    assert_raises(ArgumentError) { AzurePolicyExemptions.new(resource_id: 'some_id') }
+  end
+end
