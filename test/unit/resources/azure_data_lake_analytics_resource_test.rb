7e9d37c34c02066f7566598c06ce99e20a03c27e
sa-progress <samir.anand@progress.com>
2021-07-15 00:13:21 +0530

    resource a,unit test cases and docs added for azure_data_lake_analytics_resource

--- test/unit/resources/azure_data_lake_analytics_resource_test.rb
+++ test/unit/resources/azure_data_lake_analytics_resource_test.rb
@@ -1,17 +1,17 @@
 require_relative 'helper'
-require 'azure_virtual_machine'
+require 'azure_data_lake_analytics_resource'
 
 class AzureDataLakeAnalyticsResourcesConstructorTest < Minitest::Test
   def test_empty_param_not_ok
-    assert_raises(ArgumentError) { AzureDataLakeAnalyticsResources.new }
+    assert_raises(ArgumentError) { AzureDataLakeAnalyticsResource.new }
   end
 
   # resource_provider should not be allowed.
   def test_resource_provider_not_ok
-    assert_raises(ArgumentError) { AzureDataLakeAnalyticsResources.new(resource_provider: 'some_type') }
+    assert_raises(ArgumentError) { AzureDataLakeAnalyticsResource.new(resource_provider: 'some_type') }
   end
 
   def test_resource_group
-    assert_raises(ArgumentError) { AzureDataLakeAnalyticsResources.new(name: 'my-name') }
+    assert_raises(ArgumentError) { AzureDataLakeAnalyticsResource.new(name: 'my-name') }
   end
 end
