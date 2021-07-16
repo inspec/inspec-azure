eeab62b814a9ad78851e2f372b074f836fecfc4e
Sathish <sbabu@progress.com>
2021-07-16 11:30:13 +0530

    integration AD objects
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_active_directory_object.rb
+++ test/integration/verify/controls/azure_active_directory_object.rb
@@ -1,5 +1,10 @@
+directory_object = input(:sample_directory_object, value: '')
+
 control 'azure active directory object' do
-  describe azure_active_directory_object(id: 'test id') do
+  describe azure_active_directory_object(id: directory_object) do
     it { should exist }
+    its('displayName') { should eq 'MyApps_AliBabaCloud' }
+    its('visibility') { should be_empty }
+    its('mailEnabled') { should be_falsey }
   end
 end
