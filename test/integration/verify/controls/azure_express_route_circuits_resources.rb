d3e728ab7f23cded27f1b06a5cf31af89550a2d5
sa-progress <samir.anand@progress.com>
2021-07-29 18:02:40 +0530

    updated names

--- test/integration/verify/controls/azure_express_route_circuits_resources.rb
+++ test/integration/verify/controls/azure_express_route_circuits_resources.rb
@@ -1,10 +1,10 @@
-resource_group = input('resource_group', value: nil)
-circuit_name = input('circuitName', value: nil)
-location = input('circuitLocation', value: nil)
-global_reach_enabled = input('globalReachEnabled', value: false)
-allow_global_reach = input('allowGlobalReach', value: false)
-service_provider_provisioning_state = input('serviceProviderProvisioningState', value: nil)
-gateway_manager_etag = input('gatewayManagerEtag', value: '')
+erc_resource_group = input('resource_group', value: nil)
+erc_circuit_name = input('circuitName', value: nil)
+erc_location = input('circuitLocation', value: nil)
+erc_global_reach_enabled = input('globalReachEnabled', value: false)
+erc_allow_global_reach = input('allowGlobalReach', value: false)
+erc_service_provider_provisioning_state = input('serviceProviderProvisioningState', value: nil)
+erc_gateway_manager_etag = input('gatewayManagerEtag', value: '')
 allow_classic_operations = input('allowClassicOperations', value: false)
 circuit_provisioning_state = input('circuitProvisioningState', value: nil)
 
@@ -17,19 +17,19 @@ sku_tier = input('sku_tier', value: nil)
 sku_family = input('sku_family', value: nil)
 
 control 'azure_express_route_circuits_resources' do
-  describe azure_express_route_circuits_resources(resource_group: resource_group) do
+  describe azure_express_route_circuits_resources(resource_group: erc_resource_group) do
     it { should exist }
-    its('names') { should include circuit_name }
-    its('locations') { should include location }
+    its('names') { should include erc_circuit_name }
+    its('locations') { should include erc_location }
     its('types') { should include 'Microsoft.Network/expressRouteCircuits' }
     its('provisioning_states') { should include('Succeeded') }
     its('service_provider_properties_bandwidth_in_mbps') { should include bandwidth_in_mbps }
     its('service_provider_properties_peering_locations') { should include peering_location }
     its('service_provider_properties_names') { should include service_provider_name }
-    its('service_provider_provisioning_states') { should include service_provider_provisioning_state }
-    its('global_reach_enabled') { should include global_reach_enabled }
-    its('allow_global_reach') { should include allow_global_reach }
-    its('gateway_manager_etags') { should include gateway_manager_etag }
+    its('service_provider_provisioning_states') { should include erc_service_provider_provisioning_state }
+    its('global_reach_enabled') { should include erc_global_reach_enabled }
+    its('allow_global_reach') { should include erc_allow_global_reach }
+    its('gateway_manager_etags') { should include erc_gateway_manager_etag }
     its('allow_classic_operations') { should include allow_classic_operations }
     its('circuit_provisioning_states') { should include circuit_provisioning_state }
     its('sku_names') { should include sku_name }
