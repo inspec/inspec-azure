8b3e6a7d8f7fbed26a795754404ecc8e7cc9192d
Rohit <rohit.prasad.joshi@sap.com>
2021-08-05 08:21:33 -0700

    removing legacy resource and updating 'readme'
    Signed-off-by: Rohit <rohit.prasad.joshi@sap.com>

--- libraries/azure_virtual_network_peerings.rb
+++ libraries/azure_virtual_network_peerings.rb
@@ -75,25 +75,3 @@ class AzureVirtualNetworkPeerings < AzureGenericResources
     super(AzureVirtualNetworkPeerings)
   end
 end
-
-# Provide the same functionality under the old resource name.
-# This is for backward compatibility.
-class AzurermVirtualNetworkPeerings < AzureVirtualNetworkPeerings
-  name 'azurerm_virtual_network_peerings'
-  desc 'Verifies settings for Azure Virtual Network Peerings'
-  example <<-EXAMPLE
-    azure_virtual_network_peerings(resource_group: 'example', vnet: 'virtual-network-name') do
-      it{ should exist }
-    end
-  EXAMPLE
-
-  def initialize(opts = {})
-    Inspec::Log.warn Helpers.resource_deprecation_message(@__resource_name__, AzurermVirtualNetworkPeerings.name)
-    # Options should be Hash type. Otherwise Ruby will raise an error when we try to access the keys.
-    raise ArgumentError, 'Parameters must be provided in an Hash object.' unless opts.is_a?(Hash)
-
-    # For backward compatibility.
-    opts[:api_version] ||= '2018-02-01'
-    super
-  end
-end
