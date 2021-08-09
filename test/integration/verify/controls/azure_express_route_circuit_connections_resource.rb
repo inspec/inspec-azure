5549230813e9aa1358ca3d5963112ddf4d465aa8
sa-progress <samir.anand@progress.com>
2021-08-09 15:08:54 +0530

    test cases added

--- test/integration/verify/controls/azure_express_route_circuit_connections_resource.rb
+++ test/integration/verify/controls/azure_express_route_circuit_connections_resource.rb
@@ -1,9 +1,12 @@
 resource_group = input('resource_group', value: nil)
 circuit_name = input('circuitName', value: nil)
 location = input('circuitLocation', value: nil)
+peering_name = input('peeringName', value: nil)
+connection_name = input('connectionName', value: nil)
 
 control 'azure_express_route_circuit_connections_resource' do
-  describe azure_express_route_circuit_connections_resource(resource_group: resource_group, circuit_name: circuit_name) do
+  describe azure_express_route_circuit_connections_resource(resource_group: resource_group, circuit_name: circuit_name,
+                                                            peering_name: peering_name, connection_name: connection_name) do
     it { should exist }
     its('name') { should eq circuit_name }
     its('type') { should eq 'Microsoft.Network/expressRouteCircuits' }
@@ -13,11 +16,13 @@ control 'azure_express_route_circuit_connections_resource' do
     its('circuit_connection_status') { should include 'Connected' }
   end
 
-  describe azure_express_route_circuit_connections_resource(resource_group: resource_group, circuit_name: 'fake') do
+  describe azure_express_route_circuit_connections_resource(resource_group: resource_group, circuit_name: circuit_name,
+                                                            peering_name: peering_name, connection_name: connection_name) do
     it { should_not exist }
   end
 
-  describe azure_express_route_circuit_connections_resource(resource_group: 'fake', circuit_name: 'cn', peering_name: 'should_not_exist') do
+  describe azure_express_route_circuit_connections_resource(resource_group: resource_group, circuit_name: circuit_name,
+                                                            peering_name: peering_name, connection_name: 'does_not_exist') do
     it { should_not exist }
   end
 end
