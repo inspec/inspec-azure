c90bc6c8a9ea4397f155c7c5bae1d0c0ab984e3a
sa-progress <samir.anand@progress.com>
2021-08-20 15:48:49 +0530

    adding pipeline resource
    Signed-off-by: sa-progress <samir.anand@progress.com>

---
+++ libraries/azure_data_factory_pipeline.rb
@@ -0,0 +1,31 @@
+require 'azure_generic_resource'
+
+class AzureDataFactoriesPipeline < AzureGenericResource
+  name 'azure_data_factory_pipeline'
+  desc 'get azure gateway unr ta factory.'
+  example <<-EXAMPLE
+      describe azure_data_factory_gateway(resource_group: resource_group, factory_name: factory_name, pipeline_name: pipeline_name)  do
+        it { should exist }
+      end
+  EXAMPLE
+
+  def initialize(opts = {})
+    # Options should be Hash type. Otherwise Ruby will raise an error when we try to access the keys.
+    raise ArgumentError, 'Parameters must be provided in an Hash object.' unless opts.is_a?(Hash)
+    #
+    # Azure REST API endpoint URL format for the resource:
+    #   GET https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/
+    #   providers/Microsoft.DataFactory/factories/{factoryName}/pipelines/{pipelineName}?api-version=2018-06-01
+    #
+    opts[:resource_provider] = specific_resource_constraint('Microsoft.DataFactory/factories', opts)
+    opts[:required_parameters] = %i(factory_name)
+    opts[:resource_path] = [opts[:factory_name], 'pipelines'].join('/')
+    opts[:resource_identifiers] = %i(pipeline_name)
+    # static_resource parameter must be true for setting the resource_provider in the backend.
+    super(opts, true)
+  end
+
+  def to_s
+    super(AzureDataFactoriesPipeline)
+  end
+end
