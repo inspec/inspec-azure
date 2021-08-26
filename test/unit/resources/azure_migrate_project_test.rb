5b0667d0fc5fae3541a48984c6ac3b6bd511c19f
Sathish <sbabu@progress.com>
2021-08-26 11:29:39 +0530

    test azure migrate project
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ test/unit/resources/azure_migrate_project_test.rb
@@ -0,0 +1,17 @@
+require_relative 'helper'
+require 'azure_migrate_project'
+
+class AzureMigrateProjectConstructorTest < Minitest::Test
+  def test_empty_param_not_ok
+    assert_raises(ArgumentError) { AzureMigrateProject.new }
+  end
+
+  # resource_provider should not be allowed.
+  def test_resource_provider_not_ok
+    assert_raises(ArgumentError) { AzureMigrateProject.new(resource_provider: 'some_type') }
+  end
+
+  def test_resource_group_name_alone_ok
+    assert_raises(ArgumentError) { AzureMigrateProject.new(name: 'my-name', resource_group: 'test') }
+  end
+end
