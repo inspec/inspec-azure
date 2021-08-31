1d1ce70afc97317f94adfce7fa249302b211b82d
Sathish <sbabu@progress.com>
2021-08-31 14:26:07 +0530

    integral azure migrate project event (s)
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ test/integration/verify/controls/azure_migrate_project_event.rb
@@ -0,0 +1,11 @@
+resource_group = input(:resource_group, value: '')
+project_name = input(:project_name, value: 'inspec-migrate-integ')
+event_name = 'c042be9e-3d93-42cf-917f-b92c68318ded'
+
+control 'test the properties of an azure migrate project event' do
+  describe azure_migrate_project_machine(resource_group: resource_group, project_name: project_name, name: event_name) do
+    it { should exist }
+    its('type') { should eq 'Microsoft.Migrate/MigrateProjects/MigrateEvents' }
+    its('properties.instanceType') { should eq 'Servers' }
+  end
+end
