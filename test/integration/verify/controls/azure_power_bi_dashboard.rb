ff3d3df4fd8955fa4cf5d8765c78ee324784b3d2
Sathish <sbabu@progress.com>
2021-09-29 09:18:11 +0530

    integral test power BI dashboard (s)
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_power_bi_dashboard.rb
+++ test/integration/verify/controls/azure_power_bi_dashboard.rb
@@ -1,7 +1,10 @@
 group_id = '95a4871a-33a4-4f35-9eea-8ff006b4840b'
-dashboard_id = '95a4871a-33a4-4f35'
+dashboard_id = 'b84b01c6-3262-4671-bdc8-ff99becf2a0b'
 control 'Verify settings of a Power BI Dashboard' do
   describe azure_power_bi_dashboard(group_id: group_id, dashboard_id: dashboard_id) do
     it { should exist }
+    its('displayName') { should eq 'inspec-dev-dashboard' }
+    its('isReadOnly') { should be_truthy }
+    its('users') { should be_empty }
   end
 end
--- test/integration/verify/controls/azure_power_bi_dashboards.rb
+++ test/integration/verify/controls/azure_power_bi_dashboards.rb
@@ -2,5 +2,8 @@ group_id = '95a4871a-33a4-4f35-9eea-8ff006b4840b'
 control 'verify settings of Power BI Dashboards in a group' do
   describe azure_power_bi_dashboards(group_id: group_id) do
     it { should exist }
+    its('displayNames') { should include 'inspec-dev-dashboard' }
+    its('isReadOnlies') { should include true }
+    its('users') { should be_empty }
   end
 end
