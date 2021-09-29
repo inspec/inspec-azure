f4f4ca474cac6bbb3786b917d86333b8da01baa3
sa-progress <samir.anand@progress.com>
2021-09-29 20:37:59 +0530

    lint
    Signed-off-by: sa-progress <samir.anand@progress.com>

--- test/integration/verify/controls/azure_data_factory_pipeline_run_resources.rb
+++ test/integration/verify/controls/azure_data_factory_pipeline_run_resources.rb
@@ -1,5 +1,5 @@
-resource_group = input('resource_group', value: "nirbhay-cosmos")
-factory_name = input('factory_name', value: "test-samir-df")
+resource_group = input('resource_group', value: 'nirbhay-cosmos')
+factory_name = input('factory_name', value: 'test-samir-df')
 
 control 'azure_data_factory_pipeline_run_resources' do
   describe azure_data_factory_pipeline_run_resources(resource_group: resource_group, factory_name: factory_name) do
