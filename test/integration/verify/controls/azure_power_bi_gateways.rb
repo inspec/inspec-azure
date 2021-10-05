77d930a38bcfe3136810190eab453c67d81016d0
Sathish <sbabu@progress.com>
2021-10-05 09:53:54 +0530

    update tests
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_power_bi_gateways.rb
+++ test/integration/verify/controls/azure_power_bi_gateways.rb
@@ -1,1 +1,7 @@
-
+control 'verify settings of Power BI Gateways' do
+  describe azure_power_bi_gateways do
+    it { should exist }
+    its('types') { should include 'Resource' }
+    its('exponents') { should include 'AQAB' }
+  end
+end
--- test/unit/resources/azure_power_bi_gateway_test.rb
+++ test/unit/resources/azure_power_bi_gateway_test.rb
@@ -1,1 +1,17 @@
+require_relative 'helper'
+require 'azure_power_bi_gateway'
 
+class AzurePowerBIGatewayConstructorTest < Minitest::Test
+  def test_empty_param_not_ok
+    assert_raises(ArgumentError) { AzurePowerBIGateway.new }
+  end
+
+  # resource_provider should not be allowed.
+  def test_resource_provider_not_ok
+    assert_raises(ArgumentError) { AzurePowerBIGateway.new(resource_provider: 'some_type') }
+  end
+
+  def test_resource_group_name_alone_ok
+    assert_raises(ArgumentError) { AzurePowerBIGateway.new(name: 'my-name', resource_group: 'test') }
+  end
+end
