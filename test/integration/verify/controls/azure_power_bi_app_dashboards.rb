6d9a41e971e1edf69fa6253ef6f4c472c81be55c
Sathish <sbabu@progress.com>
2021-10-11 10:32:19 +0530

    add more integration tests
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_power_bi_app_dashboards.rb
+++ test/integration/verify/controls/azure_power_bi_app_dashboards.rb
@@ -2,5 +2,6 @@ app_id = input(:app_id, value: '')
 control 'Verify the settings of all Power BI App Dashboards' do
   describe azure_power_bi_app_dashboards(app_id: app_id) do
     it { should exist }
+    its('names') { should include 'inspec-bi-dashboard' }
   end
 end
