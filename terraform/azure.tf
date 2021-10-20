082edf7d858912d1ed294a481627ece6868ab513
Sathish <sbabu@progress.com>
2021-10-20 11:26:21 +0530

    tf support vnet gateway
    Signed-off-by: Sathish <sbabu@progress.com>

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -1498,3 +1498,15 @@ EOF
     }
   }
 }
+
+resource "azurerm_virtual_network_gateway_connection" "nw-gateway-connection" {
+  name                = "inspec-nw-gateway-connection"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  type                            = "Vnet2Vnet"
+  virtual_network_gateway_id      = azurerm_virtual_network_gateway.inspec-nw-gateway.id
+  peer_virtual_network_gateway_id = azurerm_virtual_network_gateway.inspec-nw-gateway.id
+
+  shared_key = "4-v3ry-53cr37-1p53c-5h4r3d-k3y"
+}
