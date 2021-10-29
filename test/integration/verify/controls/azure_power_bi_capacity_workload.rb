5938575697cb0384f43a6005f84b87c09cd94274
Sathish <sbabu@progress.com>
2021-10-29 12:51:23 +0530

    update test keys
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_power_bi_capacity_workload.rb
+++ test/integration/verify/controls/azure_power_bi_capacity_workload.rb
@@ -1,9 +1,8 @@
 control 'verify the settings of a Azure Power BI Capacity Workloads' do
   describe azure_power_bi_capacity_workload(capacity_id: '0f084df7-c13d-451b-af5f-ed0c466403b2', name: 'Dataflows') do
     it { should exist }
-    its('refreshesPerDays') { should include 11 }
-    its('refreshCounts') { should include 22 }
-    its('kinds') { should include 'Dataset' }
-    its('refreshSchedules') { should_not be empty }
+    its('state') { should eq 'Enabled' }
+    its('name') { should eq 'Dataflows' }
+    its('maxMemoryPercentageSetByUser') { should eq '66' }
   end
 end
--- test/integration/verify/controls/azure_power_bi_capacity_workloads.rb
+++ test/integration/verify/controls/azure_power_bi_capacity_workloads.rb
@@ -1,9 +1,8 @@
 control 'verify the settings of all Azure Power BI Capacity Workloads' do
   describe azure_power_bi_capacity_workloads(capacity_id: '0f084df7-c13d-451b-af5f-ed0c466403b2') do
     it { should exist }
-    its('refreshesPerDays') { should include 11 }
-    its('refreshCounts') { should include 22 }
-    its('kinds') { should include 'Dataset' }
-    its('refreshSchedules') { should_not be empty }
+    its('states') { should include 'Enabled' }
+    its('names') { should include 'Dataflows' }
+    its('maxMemoryPercentageSetByUsers') { should include '66' }
   end
 end
