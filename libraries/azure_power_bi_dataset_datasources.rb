db24e42eb3632b08721b8184a0d45acde3ad4e6f
Sathish <sbabu@progress.com>
2021-11-09 09:42:49 +0530

    support power BI dataset datasources
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ libraries/azure_power_bi_dataset_datasources.rb
@@ -0,0 +1,39 @@
+require 'azure_generic_resources'
+
+class AzurePowerBIDatasetDatasources < AzureGenericResources
+  name 'azure_power_bi_dataset_datasources'
+  desc 'Retrieves and verifies the settings of all Azure Power BI Dataset Datasources'
+  example <<-EXAMPLE
+    describe azure_power_bi_dataset_datasources(dataset_id: 'cfafbeb1-8037-4d0c-896e-a46fb27ff229') do
+      it { should exist }
+    end
+  EXAMPLE
+
+  AUDIENCE = 'https://analysis.windows.net/powerbi/api'.freeze
+
+  def initialize(opts = {})
+    raise ArgumentError, 'Parameters must be provided in an Hash object.' unless opts.is_a?(Hash)
+    Validators.validate_parameters(resource_name: @__resource_name__, allow: %i(group_id), required: %i(dataset_id),
+                                   opts: opts)
+
+    opts[:resource_uri] = ['https://api.powerbi.com/v1.0/myorg'].tap do |array|
+      if opts[:group_id].present?
+        array << 'groups'
+        array << opts.delete(:group_id)
+      end
+      array << ['datasets', opts.delete(:dataset_id), 'datasources']
+    end.join('/')
+    opts[:audience] = AUDIENCE
+    opts[:add_subscription_id] = false
+    opts[:is_uri_a_url] = true
+    opts[:api_version] = 'v1.0'
+    super
+    return if failed_resource?
+
+    populate_filter_table_from_response
+  end
+
+  def to_s
+    super(AzurePowerBIDatasetDatasources)
+  end
+end
