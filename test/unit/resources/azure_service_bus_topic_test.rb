e7bd04cb486825f413c1f7411cabc36e96b05c40
Sathish <sbabu@progress.com>
2021-11-23 11:27:34 +0530

    unit test service bus topic(s)
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/unit/resources/azure_service_bus_topic_test.rb
+++ test/unit/resources/azure_service_bus_topic_test.rb
@@ -1,1 +1,17 @@
+require_relative 'helper'
+require 'azure_service_bus_topic'
 
+class AzureServiceBusTopicConstructorTest < Minitest::Test
+  def test_empty_param_not_ok
+    assert_raises(ArgumentError) { AzureServiceBusTopic.new }
+  end
+
+  # resource_provider should not be allowed.
+  def test_resource_provider_not_ok
+    assert_raises(ArgumentError) { AzureServiceBusTopic.new(resource_provider: 'some_type') }
+  end
+
+  def test_resource_group_name_alone_not_ok
+    assert_raises(ArgumentError) { AzureServiceBusTopic.new(resource_group: 'test') }
+  end
+end
