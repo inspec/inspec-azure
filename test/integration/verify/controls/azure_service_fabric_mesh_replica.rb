e70fb5bbf066721de44d81c2d970b2523c90eef7
Sathish <sbabu@progress.com>
2021-12-14 11:16:31 +0530

    integral test service fabric mesh replica(s)
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ test/integration/verify/controls/azure_service_fabric_mesh_replica.rb
@@ -0,0 +1,8 @@
+control 'test the properties of all Azure Service Fabric Mesh Service Replica' do
+  describe azure_service_fabric_mesh_replicas(resource_group: 'inspec-def-rg', application_name: 'inspec-fabric-app-name', service_name: 'inspec-fabric-svc', name: 'inspec-fabric-replica') do
+    it { should exist }
+    its('osType') { should eq 'Linux' }
+    its('networkRefs') { should_not be_empty }
+    its('replicaName') { should eq 1 }
+  end
+end
