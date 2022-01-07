06428561e57160878293a7c2c447ccc7f811e6b9
Sathish <sbabu@progress.com>
2022-01-07 11:29:24 +0530

    remove all references of old resource - api management
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_api_management.rb
+++ test/integration/verify/controls/azure_api_management.rb
@@ -1,9 +1,9 @@
 resource_group = attribute('resource_group', default: nil)
 api_management_name = attribute('api_management_name', default: '')
 
-control 'azurerm_api_management' do
+control 'azure_api_management' do
   only_if { !api_management_name.empty? }
-  describe azurerm_api_management(resource_group: resource_group, api_management_name: api_management_name) do
+  describe azure_api_management(resource_group: resource_group, api_management_name: api_management_name) do
     it                { should exist }
     its('id')         { should_not be_nil }
     its('name')       { should eq api_management_name }
--- test/integration/verify/controls/azure_api_managements.rb
+++ test/integration/verify/controls/azure_api_managements.rb
@@ -1,14 +1,14 @@
 resource_group = attribute('resource_group', default: nil)
 api_management_name = attribute('api_management_name', default: '')
 
-control 'azurerm_api_managements' do
+control 'azure_api_managements' do
   only_if { !api_management_name.empty? }
-  describe azurerm_api_managements(resource_group: resource_group) do
+  describe azure_api_managements(resource_group: resource_group) do
     it           { should exist }
     its('names') { should include api_management_name }
   end
 
-  describe azurerm_api_managements do
+  describe azure_api_managements do
     it            { should exist }
     its('names')  { should include api_management_name }
   end
