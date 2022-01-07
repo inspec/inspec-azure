69a47ac12f5c0ac005d3dc06bd7a5a4293fa2f28
Sathish <sbabu@progress.com>
2022-01-07 11:25:07 +0530

    rename to azure Graph User
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_graph_user.rb
+++ test/integration/verify/controls/azure_graph_user.rb
@@ -1,7 +1,7 @@
-control 'azurerm_ad_user' do
+control 'azure_graph_user' do
 
-  user_id = azurerm_ad_users.object_ids.first
-  describe azurerm_ad_user(user_id: user_id) do
+  user_id = azure_graph_users.object_ids.first
+  describe azure_graph_user(user_id: user_id) do
     it                    { should exist }
     its('objectId')       { should eq user_id }
     its('displayName')    { should_not be_nil }
--- test/integration/verify/controls/azure_graph_users.rb
+++ test/integration/verify/controls/azure_graph_users.rb
@@ -1,6 +1,6 @@
-control 'azurerm_ad_users' do
+control 'azure_graph_users' do
 
-  describe azurerm_ad_users do
+  describe azure_graph_users do
     its('display_names')       { should_not be_empty }
     its('user_types')          { should_not be_empty }
     its('mails')               { should_not be_empty }
