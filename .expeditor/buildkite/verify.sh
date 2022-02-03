c03db1ce84dac90445d884576a7c4f10a55a5fd3
Sathish <sbabu@progress.com>
2022-02-03 10:29:15 +0530

    remove unnecessary std outs
    Signed-off-by: Sathish <sbabu@progress.com>

--- .expeditor/buildkite/verify.sh
+++ .expeditor/buildkite/verify.sh
@@ -6,8 +6,6 @@ echo "--- system details"
 uname -a
 ruby -v
 bundle --version
-echo "--- number of cores"
-nproc
 
 echo "--- bundle install"
 bundle config set --local without tools maintenance deploy
