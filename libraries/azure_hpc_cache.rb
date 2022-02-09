6c9f9080170045e553c851990c950456c525b5f2
Sathish <sbabu@progress.com>
2022-02-09 18:45:06 +0530

    support singular HPC cache
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ libraries/azure_hpc_cache.rb
@@ -0,0 +1,22 @@
+require 'azure_generic_resource'
+
+class AzureHPCCache < AzureGenericResource
+  name 'azure_hpc_cache'
+  desc 'Retrieves and verifies the settings of an Azure HPC Cache.'
+  example <<-EXAMPLE
+    describe azure_hpc_cache(resource_group: 'inspec-def-rg', name: 'sql-machine') do
+      it { should exist }
+    end
+  EXAMPLE
+
+  def initialize(opts = {})
+    raise ArgumentError, 'Parameters must be provided in an Hash object.' unless opts.is_a?(Hash)
+
+    opts[:resource_provider] = specific_resource_constraint('Microsoft.StorageCache/caches', opts)
+    super(opts, true)
+  end
+
+  def to_s
+    super(AzureHPCCache)
+  end
+end
