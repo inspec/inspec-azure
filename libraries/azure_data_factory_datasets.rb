d43aa4d62f96845bcda6331266746ba86b4fa5e8
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-02-25 13:01:03 +0530

    lint errors fixed
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- libraries/azure_data_factory_datasets.rb
+++ libraries/azure_data_factory_datasets.rb
@@ -18,7 +18,7 @@ class AzureDataFactoryDataSets < AzureGenericResources
     opts[:resource_provider] = specific_resource_constraint('Microsoft.DataFactory/factories', opts)
     opts[:required_parameters] = %i(factory_name)
     opts[:resource_path] = [opts[:factory_name], 'datasets'].join('/')
-    
+
     # static_resource parameter must be true for setting the resource_provider in the backend.
     super(opts, true)
 
