e63a54f31537ccc0b4e3b9a2bc69c13b30afe105
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2022-03-02 17:29:20 +0530

    Update test/integration/verify/controls/azure_service_fabric_mesh_replica.rb

--- test/integration/verify/controls/azure_service_fabric_mesh_replica.rb
+++ test/integration/verify/controls/azure_service_fabric_mesh_replica.rb
@@ -1,4 +1,4 @@
-control 'test the properties of all Azure Service Fabric Mesh Service Replica' do
+skip_control 'test the properties of all Azure Service Fabric Mesh Service Replica' do
   describe azure_service_fabric_mesh_replicas(resource_group: 'inspec-def-rg', application_name: 'inspec-fabric-app-name', service_name: 'inspec-fabric-svc', name: 'inspec-fabric-replica') do
     it { should exist }
     its('osType') { should eq 'Linux' }
