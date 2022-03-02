295fe887453a8f855a3c2427ad3129025d96e1e9
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2022-03-02 17:32:42 +0530

    Update test/integration/verify/controls/azure_service_fabric_mesh_volume.rb

--- test/integration/verify/controls/azure_service_fabric_mesh_volume.rb
+++ test/integration/verify/controls/azure_service_fabric_mesh_volume.rb
@@ -1,7 +1,7 @@
 rg = input(:resource_group, value: '')
 location = input(:location, value: '')
 
-skip_control 'Test the properties of the Azure Service Fabric Mesh Application' do
+skip_control 'Test the properties of the Azure Service Fabric Mesh Volume' do
   describe azure_service_fabric_mesh_volume(resource_group: rg, name: 'mesh-volume-name') do
     it { should exist }
     its('properties.provider') { should eq 'SFAzureFile' }
