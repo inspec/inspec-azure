2c8cf137136b27cbd470cca4b7ae5d48250914f3
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2022-03-02 17:26:05 +0530

    Update test/integration/verify/controls/azure_service_fabric_mesh_application.rb

--- test/integration/verify/controls/azure_service_fabric_mesh_application.rb
+++ test/integration/verify/controls/azure_service_fabric_mesh_application.rb
@@ -1,7 +1,7 @@
 resource_group = input(:resource_group, value: '')
 location = input(:location, value: '')
 
-control 'test the properties of an Azure Service Fabric Mesh Application' do
+skip_control 'test the properties of an Azure Service Fabric Mesh Application' do
   describe azure_service_fabric_mesh_application(resource_group: resource_group, name: 'inspec-fb-mesh-app') do
     it { should exist }
     its('name') { should eq 'inspec-fb-mesh-app' }
