0c5adb3ee4792a6defa8ab6f949c628ca0115bff
Soumyodeep Karmakar <63713087+soumyo13@users.noreply.github.com>
2022-03-02 17:26:16 +0530

    Update test/integration/verify/controls/azure_service_fabric_mesh_applications.rb

--- test/integration/verify/controls/azure_service_fabric_mesh_applications.rb
+++ test/integration/verify/controls/azure_service_fabric_mesh_applications.rb
@@ -1,6 +1,6 @@
 location = input(:location, value: '')
 
-control 'test the properties of all Azure Service Fabric Mesh Applications' do
+skip_control 'test the properties of all Azure Service Fabric Mesh Applications' do
   describe azure_service_fabric_mesh_applications do
     it { should exist }
     its('names') { should include 'inspec-fb-mesh-app' }
