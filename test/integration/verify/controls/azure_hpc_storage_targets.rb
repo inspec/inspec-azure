231fdec8e2cb465c8e32fe6d81c6fb55d735f048
Sathish <sbabu@progress.com>
2022-03-24 20:39:41 +0530

    fix initeg tests
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_hpc_storage_targets.rb
+++ test/integration/verify/controls/azure_hpc_storage_targets.rb
@@ -1,13 +1,13 @@
 resource_group = input(:resource_group, value: '')
+location = input(:location, value: '')
+cache_name = input(:inspec_hpc_cache_name, value: '')
 
 control 'Verify settings of all Azure HPC Storage Targets' do
-  describe azure_hpc_storage_targets do
+  describe azure_hpc_storage_targets(resource_group: resource_group, cache_name: cache_name) do
     it { should exist }
-    its('name') { should include synapse_ws_name }
+    its('name') { should include 'st1' }
     its('locations') { should include location.downcase.gsub("\s", '') }
-    its('provisioningStates') { should include 'Succeeded' }
-    its('managedResourceGroupNames') { should include resource_group }
-    its('sqlAdministratorLogins') { should include 'login' }
+    its('states') { should include 'Ready' }
+    its('targetTypes') { should include 'nfs3' }
   end
 end
-
