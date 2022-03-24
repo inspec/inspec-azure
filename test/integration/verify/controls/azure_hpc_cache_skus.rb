3f426958c1c73ab222600814d80f98acc7aa71d0
Sathish <sbabu@progress.com>
2022-03-24 17:54:42 +0530

    integration test for hpc cache sku
    Signed-off-by: Sathish <sbabu@progress.com>

--- test/integration/verify/controls/azure_hpc_cache_skus.rb
+++ test/integration/verify/controls/azure_hpc_cache_skus.rb
@@ -1,1 +1,10 @@
+describe azure_hpc_cache_skus do
+  it { should exist }
+  its('tier') { should eq 'Standard' }
+  its('size') { should eq 'A0' }
+end
 
+describe azure_hpc_cache_skus.where(tier: 'Standard') do
+  it { should exist }
+  its('size') { should eq 'A0' }
+end
