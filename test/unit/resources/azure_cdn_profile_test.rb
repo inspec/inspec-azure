1444c0486fc06670fc83586cf164ba4d60509f11
Sathish <sbabu@progress.com>
2022-06-10 22:24:19 -0500

    support singular cdn profile

---
+++ test/unit/resources/azure_cdn_profile_test.rb
@@ -0,0 +1,12 @@
+require_relative 'helper'
+require 'azure_cdn_profile'
+
+class AzureCDNProfileConstructorTest < Minitest::Test
+  def test_empty_param_not_ok
+    assert_raises(ArgumentError) { AzureCDNProfile.new }
+  end
+
+  def test_resource_group_alone_not_ok
+    assert_raises(ArgumentError) { AzureCDNProfile.new(resource_provider: 'some_type') }
+  end
+end
