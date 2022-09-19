f81cedb7b143cbd202e7384cf41de6d8513f2b16
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-09-19 16:15:13 +0530

    added singular unit test
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/unit/resources/azure_blob_service.rb
@@ -0,0 +1,17 @@
+require_relative 'helper'
+require 'azure_blob_service'
+
+class AzureBlobServiceConstructorTest < Minitest::Test
+  # Generic resource requires a parameter.
+  def test_empty_params_not_ok
+    assert_raises(ArgumentError) { AzureBlobService.new }
+  end
+
+  def test_not_allowed_parameter
+    assert_raises(ArgumentError) { AzureBlobService.new(resource: 'domains', id: 'some_id', fake: 'random') }
+  end
+
+  def test_filter_not_allowed
+    assert_raises(ArgumentError) { AzureBlobService.new(resource: 'domains', id: 'some_id', filter: 'random') }
+  end
+end
