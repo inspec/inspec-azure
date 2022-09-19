4e94743f24a6fbebd15a4fee5abc88f6aede3321
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-09-19 16:14:29 +0530

    added singular integration test
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/azure_blob_service.rb
@@ -0,0 +1,41 @@
+resource_group = input('resource_group', value: 'samirdev', description: '')
+storage_account_blob_service_id = input('storage_account_blob_service_id', value: '/subscriptions/80b824de-ec53-4116-9868-3deeab10b0cd/resourceGroups/samirdev/providers/Microsoft.Storage/storageAccounts/samirstorage1/blobServices/default', description: '')
+storage_account_blob_service_name = input('storage_account_blob_service_name', value: 'default', description: '')
+storage_account_blob_type = input('storage_account_blob_type', value: 'Microsoft.Storage/storageAccounts/blobServices', description: '')
+storage_account = input('storage_account', value: 'samirstorage1', description: '')
+
+control 'azure_blob_service' do
+  impact 1.0
+  title 'Testing the singular resource of azure_blob_service.'
+  desc 'Testing the singular resource of azure_blob_service.'
+
+  describe azure_blob_service(resource_group: resource_group, storage_account_name: storage_account) do
+    it { should exist }
+  end
+
+  describe azure_blob_service(resource_group: resource_group, storage_account_name: storage_account) do
+    its('sku.name') { should eq 'Standard_RAGRS' }
+    its('sku.tier') { should eq 'Standard' }
+
+    its('id') { should eq storage_account_blob_service_id }
+    its('name') { should eq storage_account_blob_service_name }
+    its('type') { should eq storage_account_blob_type }
+
+    its('properties.changeFeed.enabled') { should eq true }
+
+    its('properties.restorePolicy.enabled') { should eq true }
+    its('properties.restorePolicy.days') { should eq 6 }
+    # its('properties.restorePolicy.minRestoreTime') { should eq Time.parse('2022-09-13T09:34:18.5206216Z') }
+
+    its('properties.containerDeleteRetentionPolicy.enabled') { should eq true }
+    its('properties.containerDeleteRetentionPolicy.days') { should eq 7 }
+
+    its('properties.corsRules') { should be_empty }
+
+    its('properties.deleteRetentionPolicy.allowPermanentDelete') { should eq false }
+    its('properties.deleteRetentionPolicy.enabled') { should eq true }
+    its('properties.deleteRetentionPolicy.days') { should eq 7 }
+
+    its('properties.isVersioningEnabled') { should eq true }
+  end
+end
