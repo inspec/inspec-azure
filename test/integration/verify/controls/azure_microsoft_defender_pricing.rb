04acaaeb26c7e8680eed87c1b30f94274db77e67
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-10-19 16:59:43 +0530

    adding modified test in the singular resource
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/azure_microsoft_defender_pricing.rb
+++ test/integration/verify/controls/azure_microsoft_defender_pricing.rb
@@ -17,3 +17,14 @@ control 'azure_microsoft_defender_pricing' do
     its('properties.freeTrialRemainingTime') { should eq 'PT0S' }
   end
 end
+
+control 'Test the Pricing Tier of each resources from the plural resource' do
+  title 'Checking the pricing tier.'
+  desc 'Test the Pricing Tier of each resources from the plural resource.'
+  
+  azure_microsoft_defender_pricings.entries.each do |entry|
+    describe azure_microsoft_defender_pricing(name: entry.name) do
+      its('properties.pricingTier') { should eq 'Standard' }
+    end
+  end
+end
