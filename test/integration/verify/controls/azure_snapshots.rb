83400d1710baa64ff5683fc7ee6e4b49a9d57a73
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-11-02 12:43:42 +0530

    adding the integration tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/azure_snapshots.rb
@@ -0,0 +1,23 @@
+snapshot_id = input('snapshot_id', value: '', desc: '')
+snapshot_name = input('snapshot_name', value: '', desc: '')
+snapshot_location = input('snapshot_location', value: '', desc: '')
+
+control 'azure_snapshots' do
+  impact 1.0
+  title 'Testing the plural resource of azure_snapshots.'
+  desc 'Testing the plural resource of azure_snapshots.'
+  
+  describe azure_snapshots do
+    it { should exist }
+  end
+  
+  describe azure_snapshots do
+    its('ids') { should include snapshot_id }
+    its('names') { should include snapshot_name }
+    its('types') { should include 'Microsoft.Compute/snapshots' }
+    its('locations') { should include snapshot_location }
+    its('tags') { should_not be_empty }
+    its('skus') { should_not be_empty }
+    its('properties') { should_not be_empty }
+  end
+end
