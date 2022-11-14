18870235aeeca42016746590a8c5ae17c696100f
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-11-14 14:33:59 +0530

    adding the tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/azure_microsoft_defender_pricing_setting.rb
@@ -0,0 +1,20 @@
+control 'azure_microsoft_defender_setting' do
+  impact 1.0
+  title 'Testing the singular resource of azure_microsoft_defender_setting.'
+  desc 'Testing the singular resource of azure_microsoft_defender_setting.'
+  
+  describe azure_microsoft_defender_setting(name: 'MCAS') do
+    it { should exist }
+  end
+  
+  describe azure_microsoft_defender_setting(name: 'MCAS') do
+    its('id') { should_not be_empty }
+    its('name') { should eq 'MCAS' }
+    its('type') { should eq 'Microsoft.Security/settings' }
+    its('kind') { should eq 'DataExportSettings' }
+  end
+  
+  describe azure_microsoft_defender_setting(name: 'MCAS') do
+    its('properties.enabled') { should be true }
+  end
+end
