f6263098ce38fd2bc74ad55e2b71858e69fd9289
samir anand <samir.anand@progress.com>
2022-11-17 21:10:58 +0530

    Exception handleing
    Signed-off-by: samir anand <samir.anand@progress.com>

--- libraries/azure_storage_account.rb
+++ libraries/azure_storage_account.rb
@@ -39,7 +39,7 @@ class AzureStorageAccount < AzureGenericResource
   def has_infrastructure_encryption_enabled?
     return unless exists?
     infrastructure_encryption = false
-    if !properties.encryption.requireInfrastructureEncryption
+    if !properties.encryption.requireInfrastructureEncryption.nil?
       infrastructure_encryption = properties.encryption.requireInfrastructureEncryption
     end
     infrastructure_encryption
