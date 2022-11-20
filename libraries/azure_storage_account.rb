16e71700dd139e6e6720da10823c8008069c1aaa
samir anand <samir.anand@progress.com>
2022-11-20 23:14:00 +0530

    adding new attribute
    Signed-off-by: samir anand <samir.anand@progress.com>

--- libraries/azure_storage_account.rb
+++ libraries/azure_storage_account.rb
@@ -38,6 +38,7 @@ class AzureStorageAccount < AzureGenericResource
 
   def has_infrastructure_encryption_enabled?
     return unless exists?
+
     infrastructure_encryption = false
     if !properties.encryption.requireInfrastructureEncryption.nil?
       infrastructure_encryption = properties.encryption.requireInfrastructureEncryption
