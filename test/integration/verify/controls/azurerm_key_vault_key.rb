ec7caeedd1a448dea37c9b7541f7d51842b02fdf
samir anand <samir.anand@progress.com>
2022-11-22 16:18:27 +0530

    rotation policy enabled check
    Signed-off-by: samir anand <samir.anand@progress.com>

--- test/integration/verify/controls/azurerm_key_vault_key.rb
+++ test/integration/verify/controls/azurerm_key_vault_key.rb
@@ -1,13 +1,13 @@
 vault_name = input('key_vault_name', value: nil)
 key_name   = input('key_vault_key_name', value: nil)
 
-control 'azurerm_key_vault_key' do
+control 'azure_key_vault_key' do
 
   impact 1.0
   title 'Testing the singular resource of azurerm_key_vault_key.'
   desc 'Testing the singular resource of azurerm_key_vault_key.'
 
-  describe azurerm_key_vault_key(vault_name, key_name) do
+  describe azure_key_vault_key(vault_name, key_name) do
     its('key.kid')    { should_not be_nil }
     its('attributes') { should have_attributes(enabled: true) }
     it { should have_rotation_policy_enabled }
