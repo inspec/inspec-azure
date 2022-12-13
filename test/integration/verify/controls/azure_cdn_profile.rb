c122c8954c6b0c501b01ec6959c0c086d4987ed4
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-12-13 17:26:59 +0530

    fixing lint errors
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/azure_cdn_profile.rb
+++ test/integration/verify/controls/azure_cdn_profile.rb
@@ -3,10 +3,10 @@ resource_group = input(:resource_group, value: '')
 location = input(:location, value: '')
 
 control 'azure_cdn_profile' do
-    title 'Testing the singular resource of azure_cdn_profile.'
-    desc 'Testing the singular resource of azure_cdn_profile.'
-  
-    describe azure_cdn_profile(resource_group: resource_group, name: name) do
+  title 'Testing the singular resource of azure_cdn_profile.'
+  desc 'Testing the singular resource of azure_cdn_profile.'
+
+  describe azure_cdn_profile(resource_group: resource_group, name: name) do
     it { should exist }
     its('location') { should eq location }
     its('provisioningState') { should eq 'Succeeded' }
