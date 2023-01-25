56a104ca2c96fa398defeff791818f9adf2e1dda
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2023-01-25 16:12:35 +0530

    fixing lint errors
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/azure_data_factory_linked_services.rb
+++ test/integration/verify/controls/azure_data_factory_linked_services.rb
@@ -6,18 +6,18 @@ linked_service_id = input("linked_service_id", value: "", description: "")
 control "azure_data_factory_linked_services" do
   title "Testing the plural resource of azure_data_factory_linked_services."
   desc "Testing the plural resource of azure_data_factory_linked_services."
-  
+
   describe azure_data_factory_linked_services(resource_group: resource_group, factory_name: factory_name) do
     it { should exist }
   end
-  
+
   describe azure_data_factory_linked_services(resource_group: resource_group, factory_name: factory_name) do
     its("names") { should include linked_service_name }
     its("types") { should include "Microsoft.DataFactory/factories/linkedservices" }
     its("ids") { should include linked_service_id }
     its("properties") { should_not be_empty }
   end
-  
+
   describe azure_data_factory_linked_services(resource_group: resource_group, factory_name: "not-there") do
     it { should_not exist }
   end
