0f19f02757b7a0520ab446a225338cfdafabe208
Dwayne Medeiros <dwayne.medeiros@gmail.com>
2026-02-21 08:28:22 -0600

    Add azure network private endpoint resources
    Signed-off-by: Dwayne Medeiros <dwayne.medeiros@gmail.com>

---
+++ test/unit/resources/azure_network_private_endpoint_test.rb
@@ -0,0 +1,17 @@
+require_relative "helper"
+require "azure_network_private_endpoint"
+
+class AzureNetworkPrivateEndpointConstructorTest < Minitest::Test
+  def test_empty_param_not_ok
+    assert_raises(ArgumentError) { AzureNetworkPrivateEndpoint.new }
+  end
+
+  # resource_provider should not be allowed.
+  def test_resource_provider_not_ok
+    assert_raises(ArgumentError) { AzureNetworkPrivateEndpoint.new(resource_provider: "some_type") }
+  end
+
+  def test_resource_group
+    assert_raises(ArgumentError) { AzureNetworkPrivateEndpoint.new(name: "my-name") }
+  end
+end
